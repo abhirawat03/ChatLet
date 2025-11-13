c2bd7f3fb7c61a89e51a357b5848e168648c69b5
abhirawat03 <abhirawat8076@gmail.com>
2025-11-13 12:10:24 +0530

    Add SERVER_HOST and SERVER_PORT support with .env loading

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,15 @@
 import uvicorn 
+import os 
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SERVER_HOST=os.getenv("SERVER_HOST")
+SERVER_PORT=int(os.getenv("SERVER_PORT"))
 if __name__ == "__main__":
-    uvicorn.run("app.main:app",host="127.0.0.1",port=8000,reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host=SERVER_HOST,
+        port=SERVER_PORT,
+        reload=True
+    )
